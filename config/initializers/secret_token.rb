88936603f4b389b736f15eefaabf6c7f86259d50
Jared Sim <jared@tinkerbox.com.sg>
2010-09-30 19:59:46 +0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acmeca::Application.config.secret_token = '30da33d97260c3c9bfc6bd5d242cfdee929614c648429ee066f4f4b533377cb4c0b2eb433917125b9a3a606df3f5576ae31a31f622e5f67d7776fa484fa3c69d'
