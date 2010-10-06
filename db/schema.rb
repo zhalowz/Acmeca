81da908a80f3efc5a07ac7d40b4b8fd859516805
Jared Sim <jared@tinkerbox.com.sg>
2010-10-06 23:30:27 +0800

    Decided to use Devise for authentication instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101002112522) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+ActiveRecord::Schema.define(:version => 20101006151346) do
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+=begin
   describe "GET 'new'" do
     it "should be successful" do
      get :new
@@ -14,4 +15,26 @@ describe UsersController do
       response.should have_selector("title", :content => "New User")
     end
   end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before (:each) do
+        @attr = { name => "", :email => "", :password => "",
+                   :password_confirmation => ""}
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => :attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "New User")
+      end
+
+    end
+  end
+=end
 end
