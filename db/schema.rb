dc0cd73a5308152d014d711ed613bc70e16741e8
Jared Sim <jared@tinkerbox.com.sg>
2010-10-09 14:49:47 +0800

    Added paperclip gem. Added product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101006161707) do
+ActiveRecord::Schema.define(:version => 20101009063950) do
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
