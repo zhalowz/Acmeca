82cc49f599f2f1a3f8597d3f6584164118cf71d7
Jared Sim <jared@tinkerbox.com.sg>
2010-10-09 16:00:37 +0800

    created partial _form for product 'new' and 'edit'

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,40 @@
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @title = "Edit Product"
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @title = "New Product"
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to @product
+    else
+      @title = "New product"
+      render 'new'
+    end
+  end
+
+  def update   
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:user])
+      flash[:success] = "Product updated."
+      redirect_to @product
+    else
+      @title = "Edit product"
+      render 'edit'
+    end
+  end
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,40 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
-  render_views
 
-=begin
-  describe "GET 'new'" do
-    it "should be successful" do
-     get :new
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get :new
-      response.should have_selector("title", :content => "New User")
-    end
-  end
-
-  describe "POST 'create'" do
-    describe "failure" do
-      before (:each) do
-        @attr = { name => "", :email => "", :password => "",
-                   :password_confirmation => ""}
-      end
-
-      it "should not create a user" do
-        lambda do
-          post :create, :user => :attr
-        end.should_not change(User, :count)
-      end
-
-      it "should have the right title" do
-        post :create, :user => @attr
-        response.should have_selector("title", :content => "New User")
-      end
-
-    end
-  end
-=end
 end
+
+
