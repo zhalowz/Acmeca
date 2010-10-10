8094de831f76dab81acfd1075d391078a8dd3aa6
Jared Sim <jared@tinkerbox.com.sg>
2010-10-10 19:45:48 +0800

    Added some tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,17 @@ class PagesController < ApplicationController
   def home
   end
 
-  def Services
+  def services
   end
 
-  def Gallary
+  def gallery
   end
 
-  def Products
-  end
-
-  def Contact
+  def contact
   end
 
   def send_mail
-	Emailer::deliver_contact_email(params[:email])
-	flash[:notice]="Email was succesfully sent."
-   end
-
+    Emailer::deliver_contact_email(params[:email])
+    flash[:notice]="Email was succesfully sent."
+  end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,4 +2,37 @@ require 'spec_helper'
 
 describe ProductsController do
 
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "Product" do
+    before(:each) do
+      @attr = { :name => "Example Product", :description => "This is an example of a product" }
+    end
+
+    it "should create a new instance given valid attributes" do
+      Product.create!(@attr)
+    end
+
+    it "should require a name" do
+      nameless_product = Product.new(@attr.merge(:name => ""))
+      nameless_product.should_not be_valid
+    end
+
+    it "should have a name no longer than 50 characters" do
+      long_name = 'a' * 51
+      long_name_product = Product.new(@attr.merge(:name => long_name))
+      long_name_product.should_not be_valid
+    end
+
+    it "should have a unique name" do
+      Product.create!(@attr)
+      duplicate_product = Product.new(@attr)
+      duplicate_product.should_not be_valid
+    end
+  end
 end
