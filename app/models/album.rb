8df2cea2647f6083cf9016d5a4526cff78a83833
Jared Sim <jared@tinkerbox.com.sg>
2010-10-18 17:05:13 +0800

    Trying to fix photo albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,13 +1,14 @@
 class Album < ActiveRecord::Base
   attr_accessible :name
-  validates_presence_of :name
-  has_many :photos, :dependent => :destroy
-  accepts_nested_attributes_for :photos
 
-def photo_attributes=(photo_attributes)
-  photo_attributes.each do |attributes|
-    photos.build(attributes)
+  validates :name, :presence => true
+
+  has_many :photos, :dependent => :destroy
+  accepts_nested_attributes_for :photos, :allow_destroy => true
+
+  def photo_attributes(photo_attributes)
+    photo_attributes.each do |attributes|
+      photos.build(attributes)
+    end
   end
-end
- 
 end
