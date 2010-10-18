4a0071679e2f220f6efd3b9ea85110d9aaf5d7e4
Jared Sim <jared@tinkerbox.com.sg>
2010-10-18 22:48:30 +0800

    Yeah! Albums now work

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,14 +1,9 @@
 class Album < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :photos_attributes
 
   validates :name, :presence => true
 
   has_many :photos, :dependent => :destroy
   accepts_nested_attributes_for :photos, :allow_destroy => true
 
-  def photo_attributes(photo_attributes)
-    photo_attributes.each do |attributes|
-      photos.build(attributes)
-    end
-  end
 end
