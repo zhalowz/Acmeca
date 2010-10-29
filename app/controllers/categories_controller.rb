5e57d7c91c3ffd7f5d50eaf84f594e1d83400a4b
Jared Sim <jared@tinkerbox.com.sg>
2010-10-29 12:52:26 +0800

    Git now ignores upload dir. Edited product view stylesheet

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
+
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @products = @category.products.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
+    @products = @category.products.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 12
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 
   cattr_reader :per_page
-  @@per_page = 10
+  @@per_page = 12
 
   attr_accessor :description_html
 
