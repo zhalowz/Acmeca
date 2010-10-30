62e19511afdda1786829d638dd9f912781bd7a62
Jared Sim <jared@tinkerbox.com.sg>
2010-10-30 11:13:21 +0800

    Added s3 support for photo and iphoto

--- app/models/iphoto.rb
+++ app/models/iphoto.rb
@@ -1,7 +1,11 @@
 class Iphoto < ActiveRecord::Base
   belongs_to :installation
 
-  has_attached_file :data, :styles => { :small => "100x100>", :medium => "300x300>" }
+  has_attached_file :data, :styles => { :small => "100x100>", :medium => "300x300>" },
+                    :storage => :s3,
+                    :s3_credentials => File.join(Rails.root,'config','amazon_s3.yml'),
+                    :path => ":attachment/:id/:style.:extension"
+
   validates :data,  :presence => true
   def self.destroy_pics(installation, iphotos)
     Iphoto.find(iphotos, :conditions => {:installation_id => installation}).each(&:destroy)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,12 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
 
-  has_attached_file :data, :styles => { :small => "100x100>", :medium => "300x300>" }
+  has_attached_file :data,  :styles => { :small => "100x100>", :medium => "300x300>" },
+                            :storage => :s3,
+                            :s3_credentials => File.join(Rails.root,'config','amazon_s3.yml'),
+                            :path => ":attachment/:id/:style.:extension"
+
+
   validates :data,  :presence => true
   def self.destroy_pics(album, photos)
     Photo.find(photos, :conditions => {:album_id => album}).each(&:destroy)
