ea929c645bc104da766e8d071125b0028e97d337
Jared Sim <jared@tinkerbox.com.sg>
2010-10-30 12:22:37 +0800

    Modified gallery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
 
-  has_attached_file :data,  :styles => { :small => "100x100>", :medium => "300x300>" },
+  has_attached_file :data,  :styles => { :small => "200x200>", :medium => "300x300>" },
                             :storage => :s3,
                             :s3_credentials => File.join(Rails.root,'config','amazon_s3.yml'),
                             :path => ":attachment/:id/:style.:extension"
