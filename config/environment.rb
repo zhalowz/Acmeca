5eabf23f4d0bb65d60c189b1d80a0e0fbc0c27ce
KaiYuan <hu_kai_yuan@hotmail.com>
2010-10-30 00:25:39 -0400

    Mouseover done

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Acmeca::Application.initialize!
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :authentication => 'plain',
-    :user_name => "zhalowz@gmail.com",
-    :password => "online90937114"
-}
