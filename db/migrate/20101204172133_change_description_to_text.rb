7c22eed97b40efd7d1206d758709e027f22b4d27
Jared Sim <jared@tinkerbox.com.sg>
2010-12-05 01:39:22 +0800

    Edited migration

--- db/migrate/20101204172133_change_description_to_text.rb
+++ db/migrate/20101204172133_change_description_to_text.rb
@@ -1,13 +1,13 @@
 class ChangeDescriptionToText < ActiveRecord::Migration
   def self.up
- change_column :rentals, :description, :text
- change_column :sales, :description, :text
- change_column :services, :description, :text
+    change_column :rentals, :description, :text
+    change_column :sales, :description, :text
+    change_column :services, :description, :text
   end
 
   def self.down
- change_column :rentals, :description, :text
- change_column :sales, :description, :text
- change_column :services, :description, :text
+    change_column :rentals, :description, :string
+    change_column :sales, :description, :string
+    change_column :services, :description, :string
   end
 end
