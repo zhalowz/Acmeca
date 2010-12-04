03bedb050e077079d511a4d4afe4bf0eb80d01e9
KaiYuan <hu_kai_yuan@hotmail.com>
2010-12-04 09:51:04 -0500

    email validation problem

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,11 +1,24 @@
 class Email
   include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
   attr_accessor :name, :address, :contact, :subject, :body
 
-  validates :name, :presence => true
-  validates :address, :presence => true,
-                      :email_format => true  
-  validates :subject, :presence => true
-  validates :body, :presence => true
+	validates_presence_of :name  
+        validates_format_of :address, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i  
+  	validates :subject, :presence => true
+	validates_presence_of :body  
+        validates_length_of :body, :maximum => 5000  
 
+
+	def initialize(attributes = {})
+	attributes.each do |name,value|
+		send("#{name}=", value)
+	end
+	end
+
+	def persisted?
+	false
+	end
 end
