e6f7a3c1140f8e1d3f6c252de9d4085b6f65807f
Jared Sim <jared@tinkerbox.com.sg>
2010-12-04 23:21:43 +0800

    bug

--- app/models/email.rb
+++ app/models/email.rb
@@ -5,8 +5,7 @@ class Email
   validates :name, :presence => true
   validates :address, :presence => true,
                       :email_format => true  
-  validates :subject, :presence => true,
-                      :maximum => 5000
+  validates :subject, :presence => true
   validates :body, :presence => true
 
 end
