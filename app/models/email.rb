b6e38745ccae634082ce68666ba9dcb88679c01b
Jared Sim <jared@tinkerbox.com.sg>
2010-12-04 23:15:54 +0800

    Reverted to earlier version

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,24 +1,11 @@
 class Email
   include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-
   attr_accessor :name, :address, :contact, :subject, :body
 
-	validates_presence_of :name  
-        validates_format_of :address, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i  
-  	validates :subject, :presence => true
-	validates_presence_of :body  
-        validates_length_of :body, :maximum => 5000  
+  validates :name, :presence => true
+  validates :address, :presence => true,
+                      :email_format => true  
+  validates :subject, :presence => true
+  validates :body, :presence => true
 
-
-	def initialize(attributes = {})
-	attributes.each do |name,value|
-		send("#{name}=", value)
-	end
-	end
-
-	def persisted?
-	false
-	end
 end
