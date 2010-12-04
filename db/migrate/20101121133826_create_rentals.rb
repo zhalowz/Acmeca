824943ee44dc769000e39bb31627f912668385e2
Jared Sim <jared@tinkerbox.com.sg>
2010-12-05 01:18:38 +0800

    Reverted the migration

--- db/migrate/20101121133826_create_rentals.rb
+++ db/migrate/20101121133826_create_rentals.rb
@@ -1,9 +1,11 @@
 class CreateRentals < ActiveRecord::Migration
   def self.up
     create_table :rentals do |t|
-    t.string :header
-      t.text :description
+      t.string :header
+      t.string :description
+
       t.timestamps
+    end
   end
 
   def self.down
