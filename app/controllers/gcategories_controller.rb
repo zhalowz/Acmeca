670d940eaccfed2bcc87b44cc315989992c1b4da
KaiYuan <hu_kai_yuan@hotmail.com>
2011-07-07 13:47:31 -0400

    edit controller

--- app/controllers/gcategories_controller.rb
+++ app/controllers/gcategories_controller.rb
@@ -4,5 +4,6 @@ class GcategoriesController < ApplicationController
     @gcategories = Gcategory.all
     @gcategory = Gcategory.find(params[:id])
     @albums = @gcategory.albums
+    @albums = @gcategory.albums.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 15
   end
 end
